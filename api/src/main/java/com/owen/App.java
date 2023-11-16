52c7d3e7570d7afdbeb9f2f4114df834d06c77be
Carol Abadeer <abadcaro@amazon.com>
2023-11-15 16:26:56 -0800

    Injected java 17 compatible 1p dependency to sub-module pom.xml

--- api/src/main/java/com/owen/App.java
+++ api/src/main/java/com/owen/App.java
@@ -1,13 +1,14 @@
 package com.owen;
 
-/**
- * Hello world!
- *
- */
+// injected 1p dependency compatible with java 17
+import com.example.MyUtility;
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "This class contains an injected 1p dependency that is compatible with java 17" );
+
+        String reversed = MyUtility.doNothing("Hello, World!");
+        System.out.println(reversed);
     }
 }
